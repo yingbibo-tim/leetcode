3705fd2e61c56f2acef8ba2879282b73bb721dae
应碧波 <yingbibo@canzhaoxi.com.cn>
2020-12-28 23:47:28 +0800

    feat(P1143):回溯--超时

---
+++ src/com/leet/code/traning/P1143LongestCommonSubsequence.java
@@ -0,0 +1,85 @@
+//给定两个字符串 text1 和 text2，返回这两个字符串的最长公共子序列的长度。 
+//
+// 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。 
+//例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。两个字符串的「公共子序列」是这两个字符串所共同拥有的子序列。
+// 
+//
+// 若这两个字符串没有公共子序列，则返回 0。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入：text1 = "abcde", text2 = "ace" 
+//输出：3  
+//解释：最长公共子序列是 "ace"，它的长度为 3。
+// 
+//
+// 示例 2: 
+//
+// 输入：text1 = "abc", text2 = "abc"
+//输出：3
+//解释：最长公共子序列是 "abc"，它的长度为 3。
+// 
+//
+// 示例 3: 
+//
+// 输入：text1 = "abc", text2 = "def"
+//输出：0
+//解释：两个字符串没有公共子序列，返回 0。
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= text1.length <= 1000 
+// 1 <= text2.length <= 1000 
+// 输入的字符串只含有小写英文字符。 
+// 
+// Related Topics 动态规划 
+// 👍 306 👎 0
+
+package com.leet.code.traning;
+
+//java:最长公共子序列
+public class P1143LongestCommonSubsequence {
+	public static void main(String[] args) {
+		Solution solution = new P1143LongestCommonSubsequence().new Solution();
+		System.out.println(solution.longestCommonSubsequence("pmjghexybyrgzczy", "hafcdqbgncrcbihkd"));
+	}
+
+	//leetcode submit region begin(Prohibit modification and deletion)
+	class Solution {
+		public int longestCommonSubsequence(String text1, String text2) {
+			if (text1 == null || text2 == null) {
+				return 0;
+			}
+			return dp(text1.length() - 1, text2.length() - 1, text1, text2);
+		}
+
+		private int dp(int i, int j, String text1, String text2) {
+
+			if (i < 0 || j < 0) {
+				return 0;
+			}
+			//相等的时候 往前找 同时双方舍弃掉相同元素
+			if (text1.charAt(i) == text2.charAt(j)) {
+				return dp(i - 1, j - 1, text1, text2) + 1;
+			} else {
+				//不相等的时候
+
+				//text1 字串回溯
+				int l1 = dp(i - 1, j, text1, text2);
+
+				//text2 字串回溯
+				int l2 = dp(i, j - 1, text1, text2);
+
+				//选择回溯出来的最大字串
+				return Math.max(l1, l2);
+			}
+
+		}
+	}
+}
